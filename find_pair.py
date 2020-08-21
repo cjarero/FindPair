2f20f24b9775fd85c381ff38fd8a721c0d2cb5cf
victoriagom <vgomez@medicosoft.mx>
2020-08-21 16:20:20 -0500

    Algoritmo find_pair Vicky

--- find_pair.py
+++ find_pair.py
@@ -2,6 +2,7 @@
 # 1. Encuentra el par con la suma dada en el arreglo
 # Lee el README.md antes de comenzar
 
+
 def find_pair(array, sum) -> [1,2]:
     """Arguents:
         array -- unsorter array of integers
@@ -9,3 +10,17 @@ def find_pair(array, sum) -> [1,2]:
     Retorna:
         array -- array of two elements with the integers of sum to find
     """
+
+    #Se utiliza O(n2): cuadrática.
+    i = 0
+    ban = True
+    while ban:
+
+        for j in range(len(array)):
+            if array[i] + array[j] == sum:
+                array_result=[array[i],array[j]]
+                ban = False
+                break
+        i+=1
+        
+    return array_result
