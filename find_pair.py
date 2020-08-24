9b52761584b7ff1cf64650ae59ea39a72fe8002c
Sanjuán Szklarz Wojciech Hernan <wsanjuan@redlab.com.mx>
2020-08-24 18:16:52 -0500

    Versión de busqueda de par de números en un arreglo con 2 algoritmos

--- find_pair.py
+++ find_pair.py
@@ -24,8 +24,18 @@ def find_pair(array, sum) -> [1,2]:
     Retorna:
         array -- array of two elements with the integers of sum to find
     """
-    
-#print(find_pair([1,3,4,5,6],11))
-#print(find_pair([1,3,4,5,6],11))
 
-# El orden es nlogn
+def find_pair2(array, sum) -> [1,2]:
+    hashset = set()
+    for num in array:
+        hashset.add(num)
+    for num in array:
+        if sum-num in hashset:
+            return [num,sum-num]
+    return [0,0]
+
+print(find_pair2([1,3,4,5,6],123))
+print(find_pair2([1,3,4,5,6],11))
+
+# El orden es nlogn de find_pair
+# El orden es n , podría decirse que 2 n para find_pair2, pero por que use el hashset, sería interesante la cantidad de memoria.
