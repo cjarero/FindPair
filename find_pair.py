9ac48650e9a98b20ecc214454f51d7bc30ca26c7
Sanjuán Szklarz Wojciech Hernan <wsanjuan@redlab.com.mx>
2020-08-24 11:04:20 -0500

    Versión de busqueda de par de números en un arreglo

--- find_pair.py
+++ find_pair.py
@@ -3,9 +3,27 @@
 # Lee el README.md antes de comenzar
 
 def find_pair(array, sum) -> [1,2]:
+    resultado = [0,0]
+    array.sort()
+    minimo = 0
+    maximo = len(array)-1
+    while minimo < maximo:
+        suma = array[minimo]+array[maximo]
+        if suma == sum:
+            resultado[0] = array[minimo]
+            resultado[1] = array[maximo]
+            return resultado
+        if suma < sum:
+            minimo = minimo+1
+        else:
+            maximo = maximo-1
+    return resultado
     """Arguents:
         array -- unsorter array of integers
         sum -- sum of pair of integers to find
     Retorna:
         array -- array of two elements with the integers of sum to find
     """
+    
+#print(find_pair([1,3,4,5,6],11))
+#print(find_pair([1,3,4,5,6],11))
