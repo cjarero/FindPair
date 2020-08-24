1e887296c9e2d39ddf7db4c914a28ed3e00679cc
rfrancomedicosoft <69821255+rfrancomedicosoft@users.noreply.github.com>
2020-08-24 10:41:06 -0500

    Add files via upload

--- find_pair.py
+++ find_pair.py
@@ -2,10 +2,21 @@
 # 1. Encuentra el par con la suma dada en el arreglo
 # Lee el README.md antes de comenzar
 
-def find_pair(array, sum) -> [1,2]:
-    """Arguents:
-        array -- unsorter array of integers
-        sum -- sum of pair of integers to find
-    Retorna:
-        array -- array of two elements with the integers of sum to find
-    """
+
+def find_pair(array, sum):
+    for i in range (0, len(array), 1):
+        z = array [i]
+        for j in range (i+1, len(array), 1):
+            y = array [j]
+            if z + y == sum:
+                print (z, y)
+       # array [i] = None   
+            
+            
+
+def envio_variables():
+    array = [2,4,0,6,1,7,8,3,2,5,25]
+    sum = 3
+    find_pair(array, sum)
+
+envio_variables()
